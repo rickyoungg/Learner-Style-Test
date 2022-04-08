11f56be8b048bfdc581e7657277fd5fa3ab89790
rickyoung <rickyoung1211@icloud.com>
2022-04-08 17:24:19 -0400

    inital commit

---
+++ src/main/java/com/RickYoung/Learner/Style/Test/LearnerStyleTestApplication.java
@@ -0,0 +1,13 @@
+package com.RickYoung.Learner.Style.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerStyleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerStyleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RickYoung/Learner/Style/Test/LearnerStyleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RickYoung.Learner.Style.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerStyleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
